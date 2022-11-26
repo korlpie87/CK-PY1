cd903a097e2e1bdccef6ab49f79b8b91aff0d5f9
korlpie87 <119118300+korlpie87@users.noreply.github.com>
2022-11-26 17:13:39 +0300

    Update task6.py

--- Лабораторная работа 3/task6.py
+++ Лабораторная работа 3/task6.py
@@ -8,7 +8,7 @@ for i, current_number in enumerate(list_numbers):  # перебираем все
         max_index = i  # перезаписываем индекс максимального элемента
         max_number = list_numbers[max_index]  # перезаписываем максимальное число
 
-# присвоим найденному максимальному и последнему элементу имена переменных:
+# поменяем местами элементы:
 list_numbers[9], list_numbers[-1] = list_numbers[-1], list_numbers[9]
 
 print(list_numbers)  # Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
